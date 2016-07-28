296fda11c81383dba9834398ce75c020fa92437b
aerovenky <venkatesh.iit2008@gmail.com>
2016-07-28 19:57:23 +0530

    typo correction

--- demo/todo/admin.py
+++ demo/todo/admin.py
@@ -3,11 +3,11 @@ from .models import Label, Note
 # Register your models here.
 
 class LabelDetail(admin.ModelAdmin):
-    list_dispaly = ('id','user','name',)
+    list_display = ('id','user','name',)
 
 admin.site.register(Label, LabelDetail)
 
 class NoteDetail(admin.ModelAdmin):
-    list_dispaly = ('id','user','name','body',)
+    list_display = ('id','user','name','body',)
 
 admin.site.register(Note, NoteDetail)
